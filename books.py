a8c98d6c4b579c0adfdcd8a2a87baf62d07e9e59
tembhrev <sw.vikastembhre@gmail.com>
2024-04-24 18:49:58 +0530

    add post method

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 
 app = FastAPI()
@@ -12,9 +12,9 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
-@app.get("/books")
-async def read_all_books():
-    return BOOKS
+# @app.get("/books")
+# async def read_all_books():
+#     return BOOKS
 
 
 
@@ -24,4 +24,36 @@ async def read_all_books(book_title: str):
         if book.get('title').casefold() == book_title.casefold():
             return book
 
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
     
+    return books_to_return
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+            
+    return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+
+
+
+
+
+
+
+
+
