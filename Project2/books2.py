158983bbbac50997faeeb742b5cc3c9f7bc9091f
tembhrev <sw.vikastembhre@gmail.com>
2024-04-29 09:51:01 +0530

    add field validation and json schema extra

--- Project2/books2.py
+++ Project2/books2.py
@@ -1,5 +1,7 @@
 from fastapi import Body, FastAPI
 from pydantic import BaseModel, Field
+from typing import Optional
+
 app = FastAPI()
 
 
@@ -19,11 +21,21 @@ class Book:
     
  
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = None
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    
+    class Config:
+        json_schema_extra = {
+            'example': {
+                'title': 'A new book',
+                'author': 'codingwithoutfear',
+                'description': 'A new description of a book',
+                'rating': 5
+            }
+        }
  
     
 BOOKS = [
@@ -43,4 +55,8 @@ async def read_all_books():
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
     print(type(new_book))
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+    
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
