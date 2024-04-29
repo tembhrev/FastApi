5590f9801db670ad89615eed257208b29f810acb
tembhrev <sw.vikastembhre@gmail.com>
2024-04-29 09:13:34 +0530

    post request before validation

--- Project2/books2.py
+++ Project2/books2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -30,3 +30,7 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request=Body()):
+    BOOKS.append(book_request)
