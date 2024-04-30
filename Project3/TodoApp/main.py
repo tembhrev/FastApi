1099927ffffb5e1458cbb2312344098b17777db7
tembhrev <sw.vikastembhre@gmail.com>
2024-04-30 09:55:32 +0530

    add API router and auth file

--- Project3/TodoApp/main.py
+++ Project3/TodoApp/main.py
@@ -6,10 +6,13 @@ from models import Todos
 from database import engine, SessionLocal
 from starlette import status
 from pydantic import BaseModel, Field
+from routers import auth
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
 
 def get_db():
     db = SessionLocal()
