781ef628c830b8299f012ec8779069faaa77b401
tembhrev <sw.vikastembhre@gmail.com>
2024-04-30 16:17:14 +0530

    add authentication for user

--- Project3/TodoApp/routers/auth.py
+++ Project3/TodoApp/routers/auth.py
@@ -6,6 +6,7 @@ from database import  SessionLocal
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -29,6 +30,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
 @router.get("/auth/")
 async def get_user():
     return {'user': 'authenticated'}
@@ -47,3 +56,10 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     
     db.add(create_user_model)
     db.commit()
+    
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Authentication'
+    return 'Successful Authentication'
