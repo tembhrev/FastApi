ded6979aa430440f5b2b366d7463ff493a11df95
tembhrev <sw.vikastembhre@gmail.com>
2024-04-30 07:02:50 +0530

    add endpoint bt todo id

--- Project3/TodoApp/main.py
+++ Project3/TodoApp/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, Query
 from sqlalchemy.orm import Session
 import models
 from typing import Annotated
 from models import Todos
 from database import engine, SessionLocal
+from starlette import status
 
 app = FastAPI()
 
@@ -18,9 +19,16 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
        
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')
 
 
 
