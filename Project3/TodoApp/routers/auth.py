2131ac17142ce4b5dd0f54eb59e47f11788e1d40
tembhrev <sw.vikastembhre@gmail.com>
2024-04-30 17:02:35 +0530

    add jwt token functionality

--- Project3/TodoApp/routers/auth.py
+++ Project3/TodoApp/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, Path, Query
+from datetime import timedelta, datetime, timezone
 from typing import Annotated
 from pydantic import BaseModel
 from models import Users
@@ -7,9 +8,12 @@ from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from starlette import status
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
 
 router = APIRouter()
 
+SECRET_KEY = 'c44d1048144d339b093bf878af4b7ac5d6ad904d0632875b9c4353260badadb0'
+ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -20,6 +24,10 @@ class CreateUserRequest(BaseModel):
     last_name: str
     password: str
     role: str
+    
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 def get_db():
     db = SessionLocal()
@@ -36,11 +44,19 @@ def authenticate_user(username: str, password: str, db):
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
-    return True
+    return user
 
 @router.get("/auth/")
 async def get_user():
     return {'user': 'authenticated'}
+
+
+def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+    
+    encode = {'sub': username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
@@ -57,9 +73,11 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.add(create_user_model)
     db.commit()
     
-@router.post("/token")
+@router.post("/token", response_model=Token)
 async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        return 'Failed Authentication'
-    return 'Successful Authentication'
+        return 'Failed Authentication'    
+    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    
+    return {'access_token': token, 'token_type': 'bearer'}
