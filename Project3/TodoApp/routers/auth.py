36c1ad85c969284b6af80b4be5ba62e0a82c38f1
tembhrev <sw.vikastembhre@gmail.com>
2024-04-30 15:06:31 +0530

    add hash user's password

--- Project3/TodoApp/routers/auth.py
+++ Project3/TodoApp/routers/auth.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -25,7 +29,7 @@ async def create_user(create_user_request: CreateUserRequest):
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
     
