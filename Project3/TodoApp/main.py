42a6d6f9550838fb19b97cddc2e8db2602f79a24
tembhrev <sw.vikastembhre@gmail.com>
2024-04-30 05:51:05 +0530

    add db and create connection

--- Project3/TodoApp/main.py
+++ Project3/TodoApp/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+
+
+
