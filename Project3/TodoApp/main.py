353012d3289c70c83f7f756d8f647bbf37e97799
tembhrev <sw.vikastembhre@gmail.com>
2024-04-30 07:47:17 +0530

    add pydantic and  post endpoint

--- Project3/TodoApp/main.py
+++ Project3/TodoApp/main.py
@@ -5,6 +5,7 @@ from typing import Annotated
 from models import Todos
 from database import engine, SessionLocal
 from starlette import status
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -18,6 +19,14 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
        
 @app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
@@ -31,6 +40,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     raise HTTPException(status_code=404, detail='Todo not found.')
 
 
-
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+    
+    db.add(todo_model)
+    db.commit()
 
 
