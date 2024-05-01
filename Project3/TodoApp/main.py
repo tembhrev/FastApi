d54b916dd1fffba9a9d640499eb384198b4a38db
tembhrev <sw.vikastembhre@gmail.com>
2024-05-01 09:57:11 +0530

    add admin for todo app

--- Project3/TodoApp/main.py
+++ Project3/TodoApp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException, Path, Query
 import models
 from database import engine
-from routers import auth, todos
+from routers import auth, todos, admin
 
 app = FastAPI()
 
@@ -9,4 +9,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
 
