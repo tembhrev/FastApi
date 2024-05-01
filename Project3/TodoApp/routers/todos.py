c910d94ed1f4094fe49c262e24a58249bb1e3499
tembhrev <sw.vikastembhre@gmail.com>
2024-05-01 07:54:30 +0530

    modify put request for authentication

--- Project3/TodoApp/routers/todos.py
+++ Project3/TodoApp/routers/todos.py
@@ -58,8 +58,8 @@ async def create_todo(user: user_dependency, db: db_dependency, todo_request: To
     
     
 @router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+async def update_todo(user: user_dependency, db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail='Todo not found.')
     
